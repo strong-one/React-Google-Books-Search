c9bb0ce8dc9e5298f837832f70ecca4149b5947d
Nick Strong <nstrong92@yahoo.com>
2021-06-23 12:53:43 -0600

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const db = require("./config/connection");
 // const routes = require("./routes");
-const { ApolloServer, gql } = require("apollo-server");
+// functions to interact with
+// const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,6 +26,12 @@ if (process.env.NODE_ENV === "production") {
 
 // app.use(routes);
 
+// db.once("open", () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
