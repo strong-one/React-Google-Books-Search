1012454bc6e67c4f71ae8d4063888f40614ce2c0
Nick Strong <nstrong92@yahoo.com>
2021-06-23 17:09:23 -0600

    constructed typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,48 @@
 // models - define entry points
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Book {
+    bookId: ID
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+  input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
