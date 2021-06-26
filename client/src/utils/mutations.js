26e8c179fc5e7b261fec0b6b1560ea99d1a5e56a
Nick Strong <nstrong92@yahoo.com>
2021-06-26 08:58:27 -0600

    working on mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,7 @@
 import { gql } from "@apollo/client";
 //- `mutations.js`:
 
-// - `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
-
 // - `ADD_USER` will execute the `addUser` mutation.
-
-// - `SAVE_BOOK` will execute the `saveBook` mutation.
-
-// - `REMOVE_BOOK` will execute the `removeBook` mutation.
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -19,3 +13,23 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+// - `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+export const LOGIN_USER = gql`
+
+mutation login($email: String!, $password: String!) {
+  login (email: $email, password $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
+}`;
+
+// - `SAVE_BOOK` will execute the `saveBook` mutation.
+export const SAVE_BOOK = gql``;
+
+// - `REMOVE_BOOK` will execute the `removeBook` mutation.
+export const REMOVE_BOOK = gql``;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,6 +2,8 @@
 
 import { gql } from "@apollo/client";
 
+// query me, me is User, User takes all user data within typeDef, savedBooks is the entry point into Book, Book needs to show all data within Book typeDef
+
 export const GET_ME = gql`
   query me {
     _id
